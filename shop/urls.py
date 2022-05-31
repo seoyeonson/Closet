ba6ecc18bc75ed4bb41b8dfed29071aa925e7b64
seoyeonson <eo970721@gmail.com>
2022-05-31 09:21:58 +0900

    손서연#5.30/17:00
    1. header 완성
    2. main.html 목록 list
    3. mypage, cart 내용 추가

--- shop/urls.py
+++ shop/urls.py
@@ -3,4 +3,7 @@ from shop import views
 
 urlpatterns = [
     path('', views.main, name='main'),
+    path('product/<int:pk>/', views.product, name='product'),
+    path('mypage/', views.mypage, name='mypage'),
+    path('cart/', views.cart, name='cart'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def main(request):
     return render(request, 'index.html')
+
+def product(request, pk):
+    return render(request, 'product.html')
+
+def mypage(request):
+    return render(request, 'mypage.html')
+
+def cart(request):
+    return render(request, 'cart.html')
