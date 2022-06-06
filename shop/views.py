b2699a248f0e99294b7056e1ce4f942b4a30ad55
SoyoungHeo2 <tblskt@naver.com>
2022-06-06 15:59:43 +0900

    허소영 #06.06 15:39
    이미지 저장

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 from shop.models import Product, Product_category, User, User_order, User_order_detail, Cart, Cuppon, Customer_inquiry, Grade
 def main(request):
     return render(request, 'index.html')
@@ -12,9 +12,12 @@ def mypage(request):
 def cart(request):
     return render(request, 'cart.html')
 
-def test_images(requests, pk):
-    product = Product.objects.filter(pk=pk)
-    print(type(product))
-    return HttpResponse(f'''
-        <h2>Images<h2>
-        ''')
+def test_images(request, id):
+    product = Product.objects.get(pk=id)
+    # count = product.count()
+    # user = User.objects.filter(u_id=id)
+    context = {
+        'product': product,
+    }
+    return render(request, 'test_images.html', context)
+
