09541076fdb04495a1beb16b4951a12dc4b2baba
SoyoungHeo2 <tblskt@naver.com>
2022-06-07 14:36:21 +0900

    허소영 # 06.07 13:28
    더미데이터 입력 모듈 + models.py class 함수 수정

--- shop/dummy_data.py
+++ shop/dummy_data.py
@@ -14,6 +14,8 @@ def User_Insert():
 
 
 def User_order_Insert():
+    # user = User.objects.get(username='user1234'),
+
     User_order.objects.create(
         user = User.objects.get(username='user1234'),
         adress = '인천광역시 연수구 114-14',
@@ -21,5 +23,31 @@ def User_order_Insert():
         receive_phone = '010-2222-3333',
         orderstatus = 1,
         received_date = datetime.datetime.now()
+    )
+    order = User_order.objects.order_by('-user')[0]
+    product = Product.objects.get(product_num=1)
+    product_count = 5
 
+    User_order_detail.objects.create(
+        product_num = product,
+        order_num = order,
+        product_count = product_count,
+        product_price = product.product_price * product_count
+    )
+
+
+def Cart_Insert():
+    product = Product.objects.get(product_num=5)
+    user = User.objects.get(username='user1234')
+    Cart.objects.create(
+        u_id = user,
+        product_num = product,
+        product_count = 3
+    )
+
+def Cuppon_Insert():
+    Cuppon.objects.create(
+        discount_rate = 0.1,
+        cuppon_num = 1,
+        u_id = User.objects.get(username='user1234')
     )
