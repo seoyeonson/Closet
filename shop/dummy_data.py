10e47163a12f37613fbb5db12b9c31765747cfd9
SoyoungHeo2 <tblskt@naver.com>
2022-06-09 14:37:11 +0900

    허소영 # 06.09 14:35
    유저 데이터 입력 함수 수정
    상품평점 데이터 입력 함수 생성

--- shop/dummy_data.py
+++ shop/dummy_data.py
@@ -1,20 +1,32 @@
 from shop.models import Product, Product_category, User, User_order, User_order_detail, Cart, Cuppon, Customer_inquiry, Grade
 import datetime
+import random
 
 def User_Insert():
-    User.objects.create(
-        username = 'user1234',
-        password = 'user1234',
-        name = '김초록',
-        nickname = '초록',
-        email = 'green@test.com',
-        sociallogin = 'green@test.com',
-        receive_marketing = True)
+    # 유저정보 생성하는 함수입니다. 가상의 유저 300명 생성합니다.
+    for i in range(300):
+        User.objects.create(
+            username = f'user{i}',
+            password = 'password',
+            name = '홍길동',
+            nickname = f'nickname{str(i)}',
+            email = f'user{str(i)}@test.com',
+            sociallogin = f'user{str(i)}@test.com',
+            mobile = '010-2222-0' + str(i+100),
+            receive_marketing = True if i % 2 == 0 else False )
 
+def Grade_Insert():
+    for i in range(1, 301): # 300명의 전체 유저가, 10개의 각각다른 상품(랜덤)에 대해 평점(랜덤)을 매깁니다. 
+        for j in range(10):
+            Grade.objects.create(
+                u_id = User.objects.get(u_id=i),
+                product_num = Product.objects.get(product_num=random.randint(1, 8315)),
+                grade = random.randint(0, 5)
+            )
 
 
 def User_order_Insert():
-    # user = User.objects.get(username='user1234'),
+    # 유저의 주문정보 데이터를 입력하는 함수입니다. 함수 실행할때마다 1개의 주문이 입력됩니다.
 
     User_order.objects.create(
         user = User.objects.get(username='user1234'),
@@ -37,6 +49,7 @@ def User_order_Insert():
 
 
 def Cart_Insert():
+    # 실행시마다 데이터 1개씩 생성됩니다. 
     product = Product.objects.get(product_num=5)
     user = User.objects.get(username='user1234')
     Cart.objects.create(
@@ -46,6 +59,7 @@ def Cart_Insert():
     )
 
 def Cuppon_Insert():
+    # 실행시마다 데이터 1개씩 생성됩니다.
     Cuppon.objects.create(
         discount_rate = 0.1,
         cuppon_num = 1,
